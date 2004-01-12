8e2bb2a4edabde8657dfd5524ae766ba878cc4f9
Rodrigo B. de Oliveira <rodrigobamboo@gmail.com>
2004-01-12 01:21:11 +0000

    GPL license
    git-svn-id: https://svn.codehaus.org/boo/trunk@67 2c1201b4-01cd-e047-a400-b836ae1fbc61

--- src/Boo.Tests/Lang/ListTestCase.cs
+++ src/Boo.Tests/Lang/ListTestCase.cs
@@ -1,3 +1,32 @@
+ï»¿#region license
+// boo - an extensible programming language for the CLI
+// Copyright (C) 2004 Rodrigo B. de Oliveira
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+// As a special exception, if you link this library with other files to
+// produce an executable, this library does not by itself cause the
+// resulting executable to be covered by the GNU General Public License.
+// This exception does not however invalidate any other reasons why the
+// executable file might be covered by the GNU General Public License.
+//
+// Contact Information
+//
+// mailto:rbo@acm.org
+#endregion
+
 using System;
 using NUnit.Framework;
 using Boo.Lang;
@@ -14,7 +43,7 @@ namespace Boo.Tests.Lang
 		[SetUp]
 		public void SetUp()
 		{
-			_list = new List("um", "dois", "três");
+			_list = new List("um", "dois", "trs");
 		}
 
 		[Test]
@@ -27,13 +56,13 @@ namespace Boo.Tests.Lang
 		public void TestAddUnique()
 		{
 			_list.AddUnique("dois");
-			AssertItems("um", "dois", "três");
+			AssertItems("um", "dois", "trs");
 		}
 
 		[Test]
 		public void TestToString()
 		{
-			AssertEquals("um, dois, três", _list.ToString());
+			AssertEquals("um, dois, trs", _list.ToString());
 		}
 
 		void AssertItems(params object[] items)
