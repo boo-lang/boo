ab451119436a6451a501fdda3199e602da60c57a
Rodrigo B. de Oliveira <rodrigobamboo@gmail.com>
2004-06-10 20:57:01 +0000

    sourceforge web files
    git-svn-id: https://svn.codehaus.org/boo/trunk@728 2c1201b4-01cd-e047-a400-b836ae1fbc61

---
+++ web/index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title>boo - a wrist friendly language for the CLI</title>
+</head>
+<body>
+<table width="100%">
+<tr>
+<td><A href="http://sourceforge.net"> <IMG src="http://sourceforge.net/sflogo.php?group_id=111475&amp;type=7" width="210" height="62" border="0" alt="SourceForge Logo"></A></td>
+<td align="right">
+	<h2>boo - a wrist friendly language for the CLI</h2>
+	<a href='http://go-mono.com/'><img border='0' src='images/mono-workswith.gif' /></a>
+</td>
+</tr> 
+</table>
+<h2>Getting Started</h2>
+Start <a href='http://boo.sourceforge.net/wiki/'>here</a>!
+<h2>Developers</h2>
+<ul>
+  <li><a href="mailto:rodrigobamboo@users.sourceforge.net">Rodrigo B. de Oliveira</a></li>
+</ul>
+<h2>Other Resources</h2>
+<p><a href="http://sourceforge.net/projects/boo">Project site at SourceForge</a></p>
+<hr width="90%" align="center" />
+<p align="center"><font size="-1">boo - a wrist friendly language for the CLI<br />
+  Copyright &copy; 2004 Rodrigo B. de Oliveira</font></p>
+
+
+</body>
+</html>
