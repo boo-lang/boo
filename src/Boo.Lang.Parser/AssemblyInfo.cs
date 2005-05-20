c396d5a2e7bd972891d00ca32fdb56a7fb8daa2b
Rodrigo B. de Oliveira <rodrigobamboo@gmail.com>
2005-05-20 14:25:21 +0000

    set class and singleton attribute now available in Useful
    git-svn-id: https://svn.codehaus.org/boo/trunk@1616 2c1201b4-01cd-e047-a400-b836ae1fbc61

--- src/Boo.Lang.Parser/AssemblyInfo.cs
+++ src/Boo.Lang.Parser/AssemblyInfo.cs
@@ -37,7 +37,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("(C) 2003-2005 Rodrigo Barreto de Oliveira")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.5.4.1613")]
+[assembly: AssemblyVersion("0.5.4.1615")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("../src/boo.snk")]
 [assembly: AssemblyKeyName("")]
