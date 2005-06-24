415a7576d286f96f280518f33c2efb4ee9be912c
Rodrigo B. de Oliveira <rodrigobamboo@gmail.com>
2005-06-24 01:23:44 +0000

    bin update after fix to latest crash bug
    git-svn-id: https://svn.codehaus.org/boo/trunk@1679 2c1201b4-01cd-e047-a400-b836ae1fbc61

--- src/Boo.Lang/AssemblyInfo.cs
+++ src/Boo.Lang/AssemblyInfo.cs
@@ -36,7 +36,7 @@ using System.Reflection;
 [assembly: AssemblyCopyright("(C) 2003-2005 Rodrigo Barreto de Oliveira")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.5.5.1673")]
+[assembly: AssemblyVersion("0.5.5.1678")]
 [assembly: AssemblyDelaySign(false)]
 #if !IGNOREKEYFILE
 [assembly: AssemblyKeyFile("../src/boo.snk")]
