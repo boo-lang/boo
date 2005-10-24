4851474f727f2838f7b4734ed668b109650cc21a
Rodrigo B. de Oliveira <rodrigobamboo@gmail.com>
2005-10-24 18:14:36 +0000

    missing file
    git-svn-id: https://svn.codehaus.org/boo/boo/trunk@1952 2c1201b4-01cd-e047-a400-b836ae1fbc61

---
+++ src/Boo.Lang.Compiler/Steps/PreProcessExtensionMethods.cs
@@ -0,0 +1,48 @@
+using System;
+using Boo.Lang.Compiler.Ast;
+
+namespace Boo.Lang.Compiler.Steps
+{
+	/// <summary>
+	/// Summary description for PreProcessExtensionMethods.
+	/// </summary>
+	public class PreProcessExtensionMethods : AbstractTransformerCompilerStep
+	{
+		public PreProcessExtensionMethods()
+		{
+		}
+
+		public override void Run()
+		{
+			Visit(CompileUnit);
+		}
+
+		public override void OnConstructor(Constructor node)
+		{
+		}
+
+		public override void OnDestructor(Destructor node)
+		{
+		}
+
+		public override void OnProperty(Property node)
+		{	
+		}
+
+		public override void OnField(Field node)
+		{	
+		}
+
+		public override void OnMethod(Boo.Lang.Compiler.Ast.Method node)
+		{
+			if (MethodImplementationFlags.Extension != (node.ImplementationFlags & MethodImplementationFlags.Extension)) return;
+
+			Visit(node.Body);
+		}
+
+		public override void OnSelfLiteralExpression(SelfLiteralExpression node)
+		{
+			ReplaceCurrentNode(new ReferenceExpression(node.LexicalInfo, "self"));
+		}
+	}
+}
