01a1422696ed4f717783193652702b822a44dcb2
Rodrigo B. de Oliveira <rodrigobamboo@gmail.com>
2006-06-11 04:25:31 +0000

    simpler version
    git-svn-id: https://svn.codehaus.org/boo/boo/trunk@2278 2c1201b4-01cd-e047-a400-b836ae1fbc61

--- src/Boo.Lang.Compiler/Steps/IntroduceGlobalNamespaces.cs
+++ src/Boo.Lang.Compiler/Steps/IntroduceGlobalNamespaces.cs
@@ -32,28 +32,18 @@ namespace Boo.Lang.Compiler.Steps
 	
 	public class IntroduceGlobalNamespaces : AbstractCompilerStep
 	{
-		public IntroduceGlobalNamespaces()
-		{
-		}
-		
 		override public void Run()
 		{
 			NameResolutionService.Reset();			
 			NameResolutionService.GlobalNamespace = new NamespaceDelegator(
 										NameResolutionService.GlobalNamespace,
-										GetGlobals());
+										SafeGetNamespace("Boo.Lang"),
+										TypeSystemServices.BuiltinsType);
 		}
 		
-		protected virtual INamespace[] GetGlobals()
+		protected INamespace SafeGetNamespace(string qname)
 		{
-			INamespace[] globals = new INamespace[2];
-			globals[0] = SafeGetNamespace((INamespace)NameResolutionService.ResolveQualifiedName("Boo.Lang"));
-			globals[1] = TypeSystemServices.BuiltinsType;
-			return globals;
-		}
-		
-		protected INamespace SafeGetNamespace(INamespace ns)
-		{
+			INamespace ns = (INamespace)NameResolutionService.ResolveQualifiedName(qname);
 			return null == ns ? NullNamespace.Default : ns;
 		}
 	}
