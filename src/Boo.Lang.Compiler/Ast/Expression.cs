d93500eb4654ab7efbaa59d8c17e1191d1489615
Cedric Vivier <cedricv@neonux.com>
2009-01-23 11:41:58 +0000

    support quasiquote expression xml serialization
    git-svn-id: https://svn.codehaus.org/boo/boo/trunk@3176 2c1201b4-01cd-e047-a400-b836ae1fbc61

--- src/Boo.Lang.Compiler/Ast/Expression.cs
+++ src/Boo.Lang.Compiler/Ast/Expression.cs
@@ -44,6 +44,7 @@ namespace Boo.Lang.Compiler.Ast
 	[System.Xml.Serialization.XmlInclude(typeof(TypeofExpression))]
 	[System.Xml.Serialization.XmlInclude(typeof(CastExpression))]
 	[System.Xml.Serialization.XmlInclude(typeof(BlockExpression))]
+	[System.Xml.Serialization.XmlInclude(typeof(QuasiquoteExpression))]
 	public abstract partial class Expression
 	{
 		public static Expression Lift(string s)
