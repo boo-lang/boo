d170540388ad17a83a9f53b66921bbe0e03d3b2a
Rodrigo B. de Oliveira <rodrigobamboo@gmail.com>
2009-12-04 17:12:12 +0000

    test case for PrimitiveParser
    git-svn-id: https://svn.codehaus.org/boo/boo/trunk@3454 2c1201b4-01cd-e047-a400-b836ae1fbc61

---
+++ tests/Boo.Lang.Parser.Tests/PrimitiveParserTest.cs
@@ -0,0 +1,16 @@
+﻿using Boo.Lang.Compiler.Ast;
+using NUnit.Framework;
+
+namespace Boo.Lang.Parser.Tests
+{
+	[TestFixture]
+	public class PrimitiveParserTest
+	{
+		[Test]
+		public void LongBoundary()
+		{
+			IntegerLiteralExpression expression = PrimitiveParser.ParseIntegerLiteralExpression(new BooToken(), "-9223372036854775808L", true);
+			Assert.AreEqual(-9223372036854775808L, expression.Value);
+		}
+	}
+}
