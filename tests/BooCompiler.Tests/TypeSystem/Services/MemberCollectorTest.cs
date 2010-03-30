28284ad8a0d5bf72c9b64502961798d2953ac54f
Rodrigo B. de Oliveira <rodrigobamboo@gmail.com>
2010-03-30 20:26:37 +0000

    missing from last checkin
    git-svn-id: https://svn.codehaus.org/boo/boo/trunk@3486 2c1201b4-01cd-e047-a400-b836ae1fbc61

---
+++ tests/BooCompiler.Tests/TypeSystem/Services/MemberCollectorTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using Boo.Lang.Compiler;
+using Boo.Lang.Compiler.TypeSystem;
+using Boo.Lang.Compiler.TypeSystem.Services;
+using NUnit.Framework;
+
+namespace BooCompiler.Tests.TypeSystem.Services
+{
+	[TestFixture]
+	public class MemberCollectorTest
+	{
+		class Foo : object
+		{	
+			public Foo() {}
+
+			public virtual string Name { get; set; }
+
+			public override string ToString() { return "Foo";  }
+		}
+
+		class Bar : Foo
+		{
+			public override string Name
+			{
+				get { return base.Name; }
+				set { base.Name = value; }
+			}
+		}
+
+		[Test]
+		public void GetAllMembers()
+		{
+			new CompilerContext().Run(ctx =>
+				{
+					var barName = typeof(Bar).FullName.Replace('+', '.');
+					var fooName = typeof(Foo).FullName.Replace('+', '.');
+					var members = MemberCollector.CollectAllMembers(My<TypeSystemServices>.Instance.Map(typeof(Bar)));
+					var expected = new[]
+					               	{
+										barName + ".constructor",
+										barName + ".get_Name",
+										barName + ".Name",
+										barName + ".set_Name",
+										fooName + ".ToString",
+                                        "object.Equals",
+                                        "object.Equals", // static
+                                        "object.GetHashCode",
+                                        "object.GetType",                                        
+                                        "object.ReferenceEquals",
+					               	};
+					var actual = members.OfType<IAccessibleMember>().Where(m => m.IsPublic).Select(m => m.FullName).ToArray();
+					Array.Sort(actual);
+					Assert.AreEqual(expected, actual);
+				});
+		}
+	}
+}
