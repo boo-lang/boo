d8cefb2c7eb682900c5150e44385feb550ff07cb
Rodrigo B. de Oliveira <rodrigobamboo@gmail.com>
2010-06-13 11:51:23 -0300

    cache type extension resolution

--- tests/BooCompiler.Tests/ExtensionsIntegrationTestFixture.cs
+++ tests/BooCompiler.Tests/ExtensionsIntegrationTestFixture.cs
@@ -120,6 +120,12 @@ namespace BooCompiler.Tests
 			RunCompilerTestCase(@"implicit-conversion-extension-2.boo");
 		}
 		
+		[Test]
+		public void per_module_extensions()
+		{
+			RunCompilerTestCase(@"per-module-extensions.boo");
+		}
+		
 
 		override protected string GetRelativeTestCasesPath()
 		{
