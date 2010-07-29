31b8d968d972ad18f072315586b44c9a2808d9e2
Rodrigo B. de Oliveira <rodrigobamboo@gmail.com>
2010-07-29 20:06:12 -0300

    unused namespaces

--- src/Boo.Lang.Compiler/CompilerWarningFactory.cs
+++ src/Boo.Lang.Compiler/CompilerWarningFactory.cs
@@ -26,8 +26,6 @@
 // THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #endregion
 
-using System;
-using System.Text;
 using Boo.Lang.Compiler.Ast;
 using Boo.Lang.Compiler.TypeSystem;
 
