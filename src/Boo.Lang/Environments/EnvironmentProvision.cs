928b524594d31717eaff299f771e3b8f56ca68c0
Rodrigo B. de Oliveira <rodrigobamboo@gmail.com>
2010-08-04 15:18:56 -0300

    introducing EnvironmentProvision<T> for cached access to environment services.

---
+++ src/Boo.Lang/Environments/EnvironmentProvision.cs
@@ -0,0 +1,14 @@
+﻿namespace Boo.Lang.Environments
+{
+	public struct EnvironmentProvision<T> where T: class
+	{
+		private T _instance;
+
+		public T Instance { get { return _instance ?? (_instance = My<T>.Instance);  } }
+
+		public static implicit operator T(EnvironmentProvision<T> provision)
+		{
+			return provision.Instance;
+		}
+	}
+}
