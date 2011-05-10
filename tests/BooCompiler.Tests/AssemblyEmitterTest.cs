578f7ff5e8da0588581283317ef9ac69dd3c8ad7
Rodrigo B. de Oliveira <rodrigobamboo@gmail.com>
2011-05-10 11:56:50 -0300

    code cleanup

---
+++ tests/BooCompiler.Tests/AssemblyEmitterTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using Boo.Lang.Compiler.Ast;
+using Boo.Lang.Compiler.MetaProgramming;
+using NUnit.Framework;
+
+namespace BooCompiler.Tests
+{
+	[TestFixture]
+	public class AssemblyEmitterTest
+	{
+		[Test]
+		[Ignore("work in progress")]
+		public void VirtualMethodsAreTaggedNewSlot()
+		{
+			var classDefinition = new ClassDefinition
+          	{
+          		Name = "Foo",
+				Members =
+				{
+					new Method
+					{
+						Name = "Bar",
+						Modifiers = TypeMemberModifiers.Virtual
+					}
+				}
+          	};
+
+			var type = Compilation.compile(classDefinition);
+			var method = type.GetMethod("Bar");
+			Assert.AreEqual(
+				MethodAttributes.NewSlot | MethodAttributes.Virtual | MethodAttributes.Public | MethodAttributes.HideBySig,
+				method.Attributes);
+		}
+	}
+}
