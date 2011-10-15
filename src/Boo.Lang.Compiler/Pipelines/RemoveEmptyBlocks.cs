b4d8e9ebc65e015d67e774c784c40c0b7d706869
Rodrigo B. de Oliveira <rodrigobamboo@gmail.com>
2011-10-15 12:46:23 -0300

    don't generate a module class just because empty blocks were left in the ast after macro expansion

---
+++ src/Boo.Lang.Compiler/Pipelines/RemoveEmptyBlocks.cs
@@ -0,0 +1,14 @@
+﻿using Boo.Lang.Compiler.Ast;
+using Boo.Lang.Compiler.Steps;
+
+namespace Boo.Lang.Compiler.Pipelines
+{
+	public class RemoveEmptyBlocks : AbstractTransformerCompilerStep
+	{
+		public override void LeaveBlock(Block node)
+		{
+			if (node.IsEmpty && node.ParentNode is Block)
+				RemoveCurrentNode();
+		}
+	}
+}
--- tests/BooCompiler.Tests/MetaProgrammingIntegrationTestFixture.cs
+++ tests/BooCompiler.Tests/MetaProgrammingIntegrationTestFixture.cs
@@ -169,6 +169,12 @@ namespace BooCompiler.Tests
 		}
 		
 		[Test]
+		public void macro_yielding_types_shouldnt_cause_module_class_to_be_defined()
+		{
+			RunCompilerTestCase(@"macro-yielding-types-shouldnt-cause-module-class-to-be-defined.boo");
+		}
+		
+		[Test]
 		public void macro_yielding_varargs()
 		{
 			RunCompilerTestCase(@"macro-yielding-varargs.boo");
