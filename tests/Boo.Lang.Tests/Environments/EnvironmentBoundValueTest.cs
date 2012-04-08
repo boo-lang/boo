c4d32e9ab2b7a41675615eac344ff12de4cfa3a8
Rodrigo B. de Oliveira <rbo@acm.org>
2012-04-08 17:39:19 -0300

    introduce EnvironmentBoundValue for threading original environment with values

---
+++ tests/Boo.Lang.Tests/Environments/EnvironmentBoundValueTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Boo.Lang.Environments;
+using NUnit.Framework;
+
+namespace Boo.Lang.Tests.Environments
+{
+	[TestFixture]
+	public class EnvironmentBoundValueTest
+	{
+		[Test]
+		public void SelectRunsInsideOriginalEnvironment()
+		{
+			var environment = new ClosedEnvironment("42");
+
+			var v = default(EnvironmentBoundValue<string>);
+			ActiveEnvironment.With(environment, () => v = EnvironmentBoundValue.Capture<string>());
+			
+			var actual = v.Select(value => Tuple.Create(value, ActiveEnvironment.Instance)).Value;
+			Assert.AreEqual("42", actual.Item1);
+			Assert.AreSame(environment, actual.Item2);
+		}
+	}
+}
