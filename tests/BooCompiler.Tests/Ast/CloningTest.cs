d290d4fb72871bc283ee4daee39d96d76db46861
Rodrigo B. de Oliveira <rbo@acm.org>
2012-07-03 15:15:51 -0300

    preserve IsSynthetic on clones

---
+++ tests/BooCompiler.Tests/Ast/CloningTest.cs
@@ -0,0 +1,19 @@
+﻿using Boo.Lang.Compiler.Ast;
+using NUnit.Framework;
+
+namespace BooCompiler.Tests.Ast
+{
+	[TestFixture]
+	public class CloningTest
+	{
+		[Test]
+		public void CloningShouldPreserveIsSynthetic()
+		{
+			foreach (var flag in new[] { true, false })
+			{
+				var node = new LabelStatement {IsSynthetic = flag};
+				Assert.AreEqual(flag, node.CloneNode().IsSynthetic);
+			}
+		}
+	}
+}
