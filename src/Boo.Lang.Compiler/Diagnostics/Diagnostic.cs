e9595ef4eba58263d4ed99d08faed6ae3163d61b
DrSlump <drslump@pollinimini.net>
2013-11-08 21:54:54 +0100

    Some specific diagnostics

--- src/Boo.Lang.Compiler/Diagnostics/Diagnostic.cs
+++ src/Boo.Lang.Compiler/Diagnostics/Diagnostic.cs
@@ -45,14 +45,59 @@ namespace Boo.Lang.Compiler.Diagnostics
 		public Hint[] Hints { get; set; }
 	}
 
+	/// <summary>
+	/// Represents a segment of the source code
+	/// </summary>
 	public class Range
 	{
-		/// TODO
+		public SourceLocation From { get; set; }
+		public SourceLocation Until { get; set; }
+
+		public Range(SourceLocation from, SourceLocation until)
+		{
+			From = from;
+			Until = until;
+		}
+
+		public Range(SourceLocation from, int length)
+		{
+			From = from;
+			Until = new SourceLocation(from.Line, from.Column + length);
+		}
+
+		public Range(Node node)
+		{
+			From = node.LexicalInfo;
+			Until = new SourceLocation(From.Line, From.Column + node.ToCodeString().Length);
+		}
 	}
 
+	/// <summary>
+	/// Represents a code modification:
+    ///  - Insert: Remove = 0, Insert = 'foo'
+    ///  - Replace: Remove = 5, Insert = 'foo'
+    ///  - Remove: Remove = 5, Insert = ''
+	/// </summary>
 	public class Hint
 	{
-		/// TODO
+	    public SourceLocation Caret { get; set; }
+    	public int Remove { get; set; }
+   	 	public string Insert { get; set; }
+
+   	 	public Hint(SourceLocation caret, int remove, string insert)
+   	 	{
+   	 		Caret = caret;
+   	 		Insert = insert;
+   	 		Remove = remove;
+   	 	}
+
+   	 	public Hint(SourceLocation caret, int remove) : this(caret, remove, null)
+   	 	{
+   	 	}
+
+   	 	public Hint(SourceLocation caret, string insert) : this(caret, 0, insert)
+   	 	{
+   	 	}
 	}
 }
 
--- src/Boo.Lang.Compiler/Diagnostics/DiagnosticFactory.cs
+++ src/Boo.Lang.Compiler/Diagnostics/DiagnosticFactory.cs
@@ -16,11 +16,62 @@ namespace Boo.Lang.Compiler.Diagnostics
 			return diag;
 		}
 
+		public static Diagnostic Warning(int code, string msg, Node node, params object[] args)
+		{
+			var diag = new Diagnostic();
+			diag.Level = DiagnosticLevel.Warning;
+			diag.Message = msg;
+			diag.Code = code;
+			diag.Caret = node.LexicalInfo;
+			diag.Arguments = args;
+			return diag;
+		}
+
 		public static Diagnostic MemberNameConflict(Node node, IType declaringType, string memberName)
 		{
-			var diag = Error(89, "{0:type} already has a definition for {1:ident}", node, declaringType, memberName);
+			var diag = Error(89, "{0:type} already has a definition for {1:id}", node, declaringType, memberName);
 			diag.Ranges = new Range[] { new Range(node.LexicalInfo, memberName.Length) };
 			return diag;
 		}
+
+		public static Diagnostic MethodSignature(Node node, IEntity expected, string actualSignature)
+		{
+			var diag = Error(17, "no overload found for {0:id} with arguments {1}", node, expected, actualSignature);
+			diag.Ranges = new Range[] { new Range(node) };
+			return diag;
+		}
+
+		public static Diagnostic InvalidOperatorForType(UnaryExpression node, string opName, IType operand)
+		{
+			var diag = Error(50, "operator {0:id} cannot be used with {1:type}", node, opName, operand);
+			diag.Ranges = new Range[] {
+				new Range(node.Operand)
+			};
+			return diag;
+		}
+
+		public static Diagnostic InvalidOperatorForTypes(BinaryExpression node, string opName, IType lhs, IType rhs)
+		{
+			var diag = Error(51, "operator {0:id} cannot be used between {1:type} and {2:type}", node, opName, lhs, rhs);
+			diag.Ranges = new Range[] {
+				new Range(node.Left),
+				new Range(node.Right)
+			};
+			return diag;
+		}
+
+		public static Diagnostic EqualsInsteadOfAssign(BinaryExpression node)
+		{
+			var diag = Warning(1007, "assignment inside a conditional", node);
+			diag.Ranges = new Range[] {
+				new Range(node.Left),
+				new Range(node.Right)
+			};
+			diag.Hints = new Hint[] {
+				new Hint(new SourceLocation(node.LexicalInfo.Line, node.LexicalInfo.Column + 1), "=")
+			};
+			return diag;
+		}
+
 	}
 }
