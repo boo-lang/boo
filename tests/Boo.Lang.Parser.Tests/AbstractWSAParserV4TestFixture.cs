8b7e286ffbacfc2d58f42ec5e952c6c94c234cab
masonwheeler <masonwheeler@yahoo.com>
2015-09-18 20:46:07 -0400

    Adding testing support for the WSA parser roundtrip suite
    Only 2 failing tests! That's off to a good start...

---
+++ tests/Boo.Lang.Parser.Tests/AbstractWSAParserV4TestFixture.cs
@@ -0,0 +1,18 @@
+﻿using Boo.Lang.Compiler.Steps;
+
+namespace Boo.Lang.Parser.Tests
+{
+	using System;
+	using Boo.Lang.Compiler;	
+	using Boo.Lang.Parser;
+
+	public class AbstractWSAParserV4TestFixture : AbstractParserTestFixture
+	{	
+		override protected CompilerPipeline CreatePipelineV4()
+		{
+			CompilerPipeline pipeline = base.CreatePipeline();
+			pipeline.Replace(typeof(Parsing), new Boo.Lang.ParserV4.WSABooParsingStep());
+			return pipeline;
+		}
+	}
+}
