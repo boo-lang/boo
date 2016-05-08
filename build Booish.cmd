378955940e16a5567839df1f960d843e8379e9de
masonwheeler <masonwheeler@yahoo.com>
2016-05-08 09:48:24 -0400

    Removing an internal file that was accidentally committed.

--- build Booish.cmd
+++
@@ -1,4 +0,0 @@
-C:\Users\Mason\Documents\nant-0.92\bin\nant booish
-.\build\booc -target:exe -o:.\build\booish.exe -srcdir:.\src\booish -reference:Boo.Lang.Interpreter.dll,Boo.Lang.Compiler.dll -platform:x86
-"C:\Program Files (x86)\Microsoft SDKs\Windows\v8.0A\bin\NETFX 4.0 Tools\CorFlags.exe" .\build\booc.exe /32BIT+ /Force
-pause
